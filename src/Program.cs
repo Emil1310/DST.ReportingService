519698a4ff1c4f16064d9a1d3e701f07e8b00ac0
Emil1310 <57002535+Emil1310@users.noreply.github.com>
2019-11-11 15:05:01 +0100

    Added method to download XSD files

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DST.ReportingService.ServiceReference;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DST.ReportingService
@@ -7,11 +9,41 @@ namespace DST.ReportingService
     {
         public static async Task Main(string[] args)
         {
-            //var client = new ReportingServiceClient();
+            // Get camping survey ID
+            await DownloadXsdFile("1153000", new DateTime(2019, 08, 01), new DateTime(2019, 08, 31));
 
-            //var xsdMarkup = await client.GetSurveyDefinitionAsync(new GetSurveyDefinitionRequest("1153000", new DateTime(2019, 08, 01), new DateTime(2019, 08, 31)));
+            // Get hotel survey ID
+            await DownloadXsdFile("1156000", new DateTime(2019, 08, 01), new DateTime(2019, 08, 31));
+        }
 
-            //Console.WriteLine(xsdMarkup.GetSurveyDefinitionResult);
+        /// <summary>
+        /// Helper method to download XSD files from DST. 
+        /// </summary>
+        /// <param name="surveyId">Id of survey, i.e. 1153000</param>
+        /// <param name="periodBegin">Survey begin time, i.e. DateTime(2019, 08, 01)</param>
+        /// <param name="periodEnd">Survey end time, i.e. DateTime(2019, 08, 31)</param>
+        private static async Task DownloadXsdFile(string surveyId, DateTime periodBegin, DateTime periodEnd)
+        {
+            // Create new WCF client
+            var client = new ReportingServiceClient();
+            // Path for XSD file, look in the XSD folder of the binary compiled file
+            var xsdPath = Path.Combine(Directory.GetCurrentDirectory(), "XSD", $"{surveyId}-{periodBegin.ToShortDateString()}-{periodEnd.ToShortDateString()}.xsd");
+            // Folder path of XSD file
+            var xsdFolderPath = Path.GetDirectoryName(xsdPath);
+              
+            // Verify folder exists, before attempting to create file
+            if (!Directory.Exists(xsdFolderPath)) {
+                Directory.CreateDirectory(xsdFolderPath);
+            }
+
+            // Create new file or re-use existing
+            using var writer = File.CreateText(xsdPath);
+            // Download XSD from DST
+            var xsdMarkup = await client.GetSurveyDefinitionAsync(new GetSurveyDefinitionRequest(surveyId, periodBegin, periodEnd));
+            // Print it in console (just for help)
+            Console.WriteLine(xsdMarkup.GetSurveyDefinitionResult);
+            // Write the actual content to the XSD file
+            writer.Write(xsdMarkup.GetSurveyDefinitionResult);
         }
     }
 }
