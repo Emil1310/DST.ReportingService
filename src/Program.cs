5afb136f973a332e656ef8fe17d45c56b05863f7
Emil1310 <57002535+Emil1310@users.noreply.github.com>
2019-11-11 14:37:52 +0100

    Added source and project files.

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DST.ReportingService
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //var client = new ReportingServiceClient();
+
+            //var xsdMarkup = await client.GetSurveyDefinitionAsync(new GetSurveyDefinitionRequest("1153000", new DateTime(2019, 08, 01), new DateTime(2019, 08, 31)));
+
+            //Console.WriteLine(xsdMarkup.GetSurveyDefinitionResult);
+        }
+    }
+}
